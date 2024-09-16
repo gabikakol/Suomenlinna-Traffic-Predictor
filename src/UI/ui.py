d78db652626fdca712e13ca3a6628968d262beea
gabikakol <gabriela.kakol@helsinki.fi>
2024-09-16 15:46:46 +0300

    UI started

---
+++ src/UI/ui.py
@@ -0,0 +1,9 @@
+class UI:
+
+    def __init__(self, root):
+        self._root = root
+        self.window = None
+        self.mainpage_view()
+
+    def mainpage_view(self):
+        pass
---
+++ src/index.py
@@ -0,0 +1,10 @@
+from tkinter import Tk
+
+def main():
+    window = Tk()
+    window.title("Suomenlinna Ferry Traffic Predictor")
+    window.geometry("1200x1000")
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
